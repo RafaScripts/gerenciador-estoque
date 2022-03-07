f68effd84111a7b7ada9c3fe775a463704fa0453
RafaScripts <rafaelw@rafascripts.live>
2022-03-06 23:24:00 -0300

    update
    dockerizing backend

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import Routes from './routes';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({ origin: '*', credentials: true }));
 
 app.use(express.json());
 
 app.use(Routes);
 
 
-app.listen(3333);
+app.listen(process.env.PORT || 3535);
